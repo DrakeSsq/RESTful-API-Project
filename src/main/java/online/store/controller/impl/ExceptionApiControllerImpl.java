95ee10893f30a69fa152fad878874c4d331a85b9
Daniil <daniil.ershovvv73@mail.ru>
2025-08-05 23:07:24 +0300

    add: error handling

--- src/main/java/online/store/controller/impl/ExceptionApiControllerImpl.java
+++ src/main/java/online/store/controller/impl/ExceptionApiControllerImpl.java
@@ -4,9 +4,11 @@ import online.store.controller.ExceptionApiController;
 import online.store.exception.ErrorMessage;
 import online.store.exception.ProductNotFoundException;
 import online.store.exception.UpdateProductException;
+import org.hibernate.query.sqm.PathElementException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
@@ -55,6 +57,28 @@ public class ExceptionApiControllerImpl implements ExceptionApiController {
                 .body(ErrorMessage.builder()
                         .message(exception.getMessage())
                         .status(HttpStatus.CONFLICT.value())
+                        .build());
+    }
+
+    @Override
+    public ResponseEntity<ErrorMessage> badAttribute(PathElementException exception) {
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ErrorMessage.builder()
+                        .message(exception.getMessage())
+                        .status(HttpStatus.NOT_FOUND.value())
+                        .build());
+    }
+
+    @Override
+    public ResponseEntity<ErrorMessage> badTypeAttribute(HttpMessageNotReadableException exception) {
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ErrorMessage.builder()
+                        .message(exception.getMessage())
+                        .status(HttpStatus.NOT_FOUND.value())
                         .build());
     }
 }
